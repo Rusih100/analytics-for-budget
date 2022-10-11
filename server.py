95fdb83b267ee66494833dfda77714bf29bbb23c
Rusih100 <rusih100@gmail.com>
2022-10-11 20:08:40 +1000

    Refactoring class Table

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from table import Table
 from pprint import pprint
 
+import telebot
 
 SPREADHEET_ID = '1n2YeegCltLcsHskPTohPY70aXd-7VlOj5AYTDEFyR_o'
 
-pprint(
-    Table(SPREADHEET_ID).append_expense(
-        'Тест', 'Тестовое название', 0
-    )
-)
+# pprint(
+#     Table(SPREADHEET_ID).append_expense(
+#         'Нулевая трата', 'Тестовоrferfwefе название', 0
+#     )
+# )
--- table.py
+++ table.py
@@ -8,12 +8,22 @@ class Table(Sheets_API):
     Класс для работы с таблицами аналитики
     """
 
-    @staticmethod
-    def get_now_time():
+    # Временная зона
+    __TIMEZONE = 'Asia/Vladivostok'
+
+    # Словарь диапазонов таблиц для функций
+    __RANGE_DICT = {
+        'MAX_ID': 'settings!H2',           # get_max_id(...)
+        'CATEGORIES': 'settings!A2:B',     # get_dict_categories(...)
+        'ID_TELEGRAM': 'settings!E2:E',    # get_list_user_id(...)
+        'APPEND_TABLE': 'input-api!A2:H',  # append_expense(...)
+    }
+
+    def get_now_time(self):
         """
         Возвращает строку текущей даты
         """
-        zone = timezone('Asia/Vladivostok')
+        zone = timezone(self.__TIMEZONE)
         now_time = datetime.now(zone)
         result = now_time.strftime('%d.%m.%Y')
         return str(result)
@@ -24,15 +34,19 @@ class Table(Sheets_API):
         """
         Получение максимального id записи из таблицы
         """
-        values = self.get_data('settings!G2')
+        values = self.get_data(
+            self.__RANGE_DICT['MAX_ID']
+        )
         max_id = int(values['values'][0][0])
         return max_id
 
-    def get_list_categories(self):
+    def get_dict_categories(self):
         """
         Получение словаря категорий трат - {<имя категории>: <id категории>}
         """
-        values = self.get_data('settings!A2:B')
+        values = self.get_data(
+            self.__RANGE_DICT['CATEGORIES']
+        )
         values = values['values']
 
         result = dict()
@@ -40,7 +54,29 @@ class Table(Sheets_API):
             result[name] = int(index)
         return result
 
+    def get_list_user_id(self):
+        """
+        Получения списка всех id пользователей telegram, у которых есть права на бота
+        """
+        values = self.get_data(
+            self.__RANGE_DICT['ID_TELEGRAM']
+        )
+        values = values['values']
+        return [int(i) for i in values]
+
     # Добавление данных в таблицы
+
+    def _get_categoty_pair(self, category: str):
+        """
+        Возвращает пару (<id категории>, <имя категории>)
+        Если такой категории трат нет в таблице вернет ('', '')
+        """
+        all_categories = self.get_dict_categories()
+
+        if category in all_categories:
+            return all_categories[category], category
+        else:
+            return '', ''
 
     def append_expense(
             self, category: str, expense_name: str, amount_expenses: int, comment: str = '', user: str = ''
@@ -48,16 +84,19 @@ class Table(Sheets_API):
         """
         Добавляет данные о трате
         """
+        category_id, category_name = self._get_categoty_pair(category)
         data = [
             [
                 self.get_max_id() + 1,
                 self.get_now_time(),
-                '',
-                category,
+                category_id,
+                category_name,
                 expense_name,
                 amount_expenses,
                 comment,
-                user
+                user  # TODO: от Telegram bot
             ]
         ]
-        return self.append_data('input-api!A2:H', data)
+        return self.append_data(
+            self.__RANGE_DICT['APPEND_TABLE'], data
+        )
