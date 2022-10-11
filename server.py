109c02d76d5a2ca70f5b28ee485fe04a4d69149b
Rusih100 <rusih100@gmail.com>
2022-10-11 20:21:38 +1000

    Loading data from .env file

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from table import Table
 from pprint import pprint
+from dotenv import load_dotenv
 
+import os
 import telebot
 
-SPREADHEET_ID = '1n2YeegCltLcsHskPTohPY70aXd-7VlOj5AYTDEFyR_o'
 
-# pprint(
-#     Table(SPREADHEET_ID).append_expense(
-#         'Нулевая трата', 'Тестовоrferfwefе название', 0
-#     )
-# )
+# Получение данных из переменных окружения
+load_dotenv()
+SPREADHEET_ID = os.getenv('SPREADHEET_ID')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+pprint(
+    Table(SPREADHEET_ID).append_expense(
+        'Нулевая трата', 'Тестовоrferfwefе название', 0
+    )
+)
