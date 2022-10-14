0ea7995c8d6bf165c7b353c3a0aadced0fc405f1
Rusih100 <rusih100@gmail.com>
2022-10-14 17:22:15 +1000

    Start aiogram telegram bot

--- server.py
+++ server.py
@@ -2,9 +2,6 @@ from dotenv import load_dotenv
 
 from table import Table
 
-from telebot.types import Message
-import telebot
-
 from pprint import pprint
 import os
 
@@ -14,15 +11,6 @@ load_dotenv()
 SPREADHEET_ID = os.getenv('SPREADHEET_ID')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
-
-@bot.message_handler(func=lambda x: True)
-def all_message(message: Message):
-    bot.send_message(chat_id=message.chat.id, text=str(message.from_user.id))
-
-
-bot.infinity_polling()
 
 # pprint(
 #     Table(SPREADHEET_ID).append_expense(
